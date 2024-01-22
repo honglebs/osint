3f3fbc35ea028028fcfd20235eadd0b44a3da14f
hong <hongle622@gmail.com>
2024-01-22 14:47:33 -0800

    updated, all libs upgraded

---
+++ modules/main.py
@@ -0,0 +1,10 @@
+from rich import print
+
+from rich.console import Console
+from rich.theme import Theme
+console = Console(theme=Theme({"repr.number": "bold green blink"}))
+console.print("The total is 128")
+
+print("Hello, [bold green]Geeks[/bold green]!")
+print("[italic red]Hello[/italic red] World!")
+
