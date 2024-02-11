0818e13a95f92bb24c8193cbb10c30daed838188
hong <hongle622@gmail.com>
2024-02-10 20:24:09 -0800

    added menu module

---
+++ main.py
@@ -0,0 +1,14 @@
+from rich import print
+
+from modules.menu import *
+
+from rich.console import Console
+
+def main():
+    print_menu()
+
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ modules/menu.py
@@ -0,0 +1,9 @@
+from rich.console import Console
+
+console = Console()
+
+def print_menu():
+    print("******** SUPER VISUALLY APPEALING *********")
+    print("******** ------------------------ *********")
+
+
