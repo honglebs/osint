2ef28389cd7023d75b131cfe06dc2f765162b41b
hong <hongle622@gmail.com>
2024-02-11 21:13:43 -0800

    added base url?

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from rich import print
-
 from modules.menu import *
-
 from rich.console import Console
+
+#this is gonna be the api we will be pulling from to see the previous spacex launches
+# check to see if we can look at future launches in the future
+BASE_API_URL = "https://api.spacexdata.com/v5/launches"
+
+
 
 def main():
     print_menu()
