34e6dc88c2837670c4645bcc31aef205c7f9052b
hong <hongle622@gmail.com>
2024-02-12 15:15:59 -0800

    more ideas

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from modules.menu import *
 from rich.console import Console
 
 #this is gonna be the api we will be pulling from to see the previous spacex launches
-# check to see if we can look at future launches in the future
+#check to see if we can look at future launches in the future
 BASE_API_URL = "https://api.spacexdata.com/v5/launches"
 
+#another idea is to have a space debris tracking? idk 
+#some other ideas 
 
+NASA_API_LINK = "https://api.nasa.gov/"
 
 def main():
     print_menu()
